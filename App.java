7ba5e32986f1c9b5fbe4f0d2972282dee4f5a682
annereyxs <111562180+annereyxs@users.noreply.github.com>
2022-09-05 10:17:25 +0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+
+                // Student Class
+                Student Me = new Student();
+        
+                Me.surname = "Reyes";
+                Me.firstname ="Jhay Anne";
+                Me.middleInitial ='P';
+                Me.dateOfBirth = "03/29/2001";
+                Me.studentNumber = 2022100473;
+                Me.studentEmailAddress = "reyesjp1@students.national-u.edu.ph";
+                Me.iAmAwesome = false;
+                Me.sayMyStudentNumber();
+                Me.sayMyEmailAddress();
+                Me.iAmAwesome();
+    }
+}
